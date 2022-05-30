50e0bd6c08e6131ff4e811c253b3fb37a1c6803f
Alexey Solovyev <solovyev.alexey@gmail.com>
2022-05-30 15:24:23 -0600

    fixture.ml: more tests

--- workspace/fixture.ml
+++ workspace/fixture.ml
@@ -1,8 +1,48 @@
 (* TODO Avoid concatenating in the next version *)
-let add x y = x + y;;
+let is_even n = n mod 2 = 0
 
 module Tests = struct
   open OUnit
-  let test1 test_ctxt = assert_equal 2 (add 1 1);;
-  let suite = ["1+1 is 2" >:: test1];;
+  let suite = [
+      "Top level test case" >:: (fun _ -> assert_equal false (is_even 1024));
+      "Test Odd" >:::
+          [
+              "Should return false for 1" >:: (fun _ -> assert_equal false (is_even 1));
+              "Should return false for 7" >:: (fun _ -> assert_equal false (is_even 7))
+          ];
+      "Test even" >:::
+          [
+              "Should return true for 100" >:: (fun _ -> assert_equal true (is_even 100));
+              "Should return true for 42" >:: (fun _ -> assert_equal true (is_even 42))
+          ];
+      "Test edge cases" >:::
+          [
+            "Test zero" >:::
+                [
+                    "Should return true for 0" >:: (fun _ -> assert_equal true (is_even 0))
+                ];
+            "Test -1" >:::
+                [
+                    "Should return false for -1" >:: (fun _ -> assert_equal false (is_even (-1)))
+                ]
+          ];
+      "Unlabeled tests" >:::
+        [
+          TestCase (fun _ -> assert_equal false (is_even 100) ~msg:"Incorrect answer for n=100");
+          TestCase (fun _ -> assert_equal false (is_even 100) ~msg:"Incorrect answer for n=100");            
+          TestList [
+            TestCase (fun _ -> assert_equal false (is_even 100) ~msg:"Incorrect answer for n=100");
+            TestCase (fun _ -> assert_equal false (is_even 100) ~msg:"Incorrect answer for n=100");            
+          ]
+          
+        ];
+      "Nested labels" >::: [
+        "Outer label" >: ("Inner label" >: ("Tests with nested labels" >::: [
+          TestCase (fun _ -> assert_equal false (is_even 100) ~msg:"Incorrect answer for n=100" ~printer: string_of_bool);
+          TestCase (fun _ -> assert_equal false (is_even 100) ~msg:"Incorrect answer for n=100" ~printer: string_of_bool);          
+          
+        ]))
+      ]
+      ]
+  ;;
 end
