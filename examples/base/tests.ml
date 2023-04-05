d58cd058821fce3b9f3a851640598264e0003473
Alexey Solovyev <solovyev.alexey@gmail.com>
2023-04-05 12:27:32 -0600

    Add the domainslib package

--- examples/base/tests.ml
+++ examples/base/tests.ml
@@ -3,11 +3,6 @@ open OUnit
 open Preloaded
 
 let suite = [
-    (* "sum tests" >:::
-        [
-            "Testing 1" >:: (fun _ -> assert_equal 1 (Solution.sum 1) ~printer:print_int);
-            "Testing 9" >:: (fun _ -> assert_equal 45 (Solution.sum 9) ~printer:print_int);
-        ]; *)
     "cubes tests (passing)" >:::
         [
             "Testing [1; 2; 3]" >:: (fun _ -> assert_equal [1; 8; 27] (Solution.cubes (List.range 1 4)) ~printer:print_int_list);
